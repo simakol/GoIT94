faa506cb8fe9c15bcfbd7a192a3267a227aceda5
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-11-07 19:23:55 +0200

    lesson-17

---
+++ script.js
@@ -0,0 +1,82 @@
+// ***********setTimeout*********** \\
+
+// const id = setTimeout(() => {
+// console.log('Time is over');
+// }, 1000)
+
+// Видалення Timeout за необхідністю
+// clearTimeout(id)
+
+// Можливість передати параметри для колбеку
+// const id = setTimeout((name, country) => {
+//     console.log(`Hello my name is ${name}, i from ${country}`);
+// }, 1000, 'Alice', 'Ukraine')
+
+// ***********setInterval*********** \\
+
+// const id = setInterval(() => {
+// console.log('setInterval');
+// }, 1000)
+
+// Видалення Timeout за необхідністю
+// clearInterval(id)
+
+// Можливість передати параметри для колбеку
+// const id = setInterval((name, country) => {
+//     console.log(`Hello my name is ${name}, i from ${country}`);
+// }, 1000, 'Alice', 'Ukraine')
+
+// Приклад використання setInterval та setTimeout
+// Створимо блок з рекламою яка зникне через 10 секунд
+
+// ***********Синхронний та асинхронний JavaScript*********** \\
+// console.log('start');
+
+// setTimeout(() => {
+//     console.log(setTimeout);
+// }, 0)
+
+// console.log('finish');
+
+// ***********Class Date*********** \\
+// const currentDate = new Date()
+// const targetDate = new Date('Sep 05 2023')
+
+// console.log(targetDate - currentDate);
+
+// Практика
+// Потрібно створити два приклади годинника (Електронний та механічний)
+// const refs = {
+//   day: document.querySelector('.date-day'),
+//   date: document.querySelector('.date'),
+//   month: document.querySelector('.date-month'),
+//   year: document.querySelector('.date-year'),
+//   clock: document.querySelector('.digital-clock'),
+//   seconds: document.querySelector('.clock-seconds__arrow'),
+//   minutes: document.querySelector('.clock-minutes__arrow'),
+//   hours: document.querySelector('.clock-hours__arrow')
+// }
+
+// const arrDay = [
+//   "Неділя",
+//   "Понеділок",
+//   "Вівторок",
+//   "Середа",
+//   "Четвер",
+//   "П`ятниця",
+//   "Субота",
+// ];
+// const namesOfMonth = [
+//   "Січень",
+//   "Лютий",
+//   "Березень",
+//   "Квітень",
+//   "Травень",
+//   "Червень",
+//   "Липень",
+//   "Серпень",
+//   "Вересень",
+//   "Жовтень",
+//   "Листопад",
+//   "Грудень",
+// ];
