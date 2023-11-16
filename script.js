6c63eda5bb95096e21f488e3e6939f168648ed1a
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-11-16 21:32:45 +0200

    lesson-20

--- script.js
+++ script.js
@@ -116,3 +116,78 @@ function createMarkup(arr) {
     )
     .join("");
 }
+
+//! =========================  Observer API ========================= !\\
+// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
+// const refs = {
+//   container: document.querySelector(".js-movie-list"),
+//   guard: document.querySelector(".js-guard"),
+// };
+
+// const options = {
+//   root: null,
+//   rootMargin: "300px",
+//   threshold: 0,
+// };
+
+// const observer = new IntersectionObserver(handlerPagination, options);
+// let page = 1;
+
+// serviceMovie()
+//   .then((data) => {
+//     selectors.container.insertAdjacentHTML("beforeend",createMarkup(data.results));
+
+//     if (data.page < data.total_pages) {
+//       observer.observe(selectors.guard);
+//     }
+//   })
+//   .catch((err) => console.log(err));
+
+// function serviceMovie(page = 1) {
+//   const BASE_URL = "https://api.themoviedb.org/3";
+//   const END_POINT = "/trending/movie/week";
+//   const API_KEY = "345007f9ab440e5b86cef51be6397df1";
+//   return fetch(`${BASE_URL}${END_POINT}?api_key=${API_KEY}&page=${page}`).then(
+//     (resp) => {
+//       if (!resp.ok) {
+//         throw new Error(resp.statusText);
+//       }
+
+//       return resp.json();
+//     }
+//   );
+// }
+
+// function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({ poster_path, release_date, original_title, vote_average }) => `
+//         <li class="movie-card">
+//           <img src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${original_title}" />
+//           <div class="movie-info">
+//               <h2>${original_title}</h2>
+//               <p>Release Date: ${release_date}</p>
+//               <p>Vote Average: ${vote_average}</p>
+//           </div>
+//          </li>`
+//     )
+//     .join("");
+// }
+
+// function handlerPagination(entries, observer) {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       page += 1;
+//       serviceMovie(page)
+//         .then((data) => {
+//           selectors.container.insertAdjacentHTML("beforeend", createMarkup(data.results));
+
+//           if (data.page >= 500) {
+//             observer.unobserve(entry.target);
+//           }
+//         })
+//         .catch((err) => console.log(err));
+//     }
+//   });
+// }
